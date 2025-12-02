69b04dcffdd2654304355a2cf5a2a62a1ba84608
Francis Hwang <sera@fhwang.net>
2025-12-01 22:40:31 -0500

    Markdown tab width is 2 chars

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -55,6 +55,17 @@ opt.timeoutlen = 300
 -- Sign column
 opt.signcolumn = "yes"
 
+-- Markdown-specific settings
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
 -- Neovide specific settings
 if g.neovide then
   -- Disable Cmd+C saving file (only copy to clipboard)
